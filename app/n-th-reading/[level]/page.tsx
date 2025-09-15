f396df4cc14133ab80ae4b594594c56614b3e2a1
SeungsooChoi <news2651@naver.com>
2025-09-15 23:27:12 +0900

    feat: 회독 ui 진행중

---
+++ app/n-th-reading/[level]/page.tsx
@@ -0,0 +1,5 @@
+export default function levelPage() {
+  return (
+    <div>TBD(50개 단어씩 테스트할 수 있도록 함)</div>
+  )
+}
--- app/n-th-reading/page.tsx
+++ app/n-th-reading/page.tsx
@@ -1,7 +1,24 @@
+import { Card, CardAction, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
+const levels = ["N5", "N4", "N3", "N2", "N1"];
+
 export default function Reading() {
   return (
-    <>
-      <div>n-th-reading</div>
-    </>
+    <div className="flex flex-col items-center gap-8">
+      <h1 className="text-xl font-bold">JLPT 레벨 선택</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        {levels.map((level) => (
+          <Card className="min-w-[100px] min-h-[100px] flex items-center justify-center shadow-lg" key={level}>
+            <CardTitle>
+              <CardAction>
+                <Link href={`n-th-reading/${level}`} className="text-lg font-semibold">{level}</Link>
+              </CardAction>
+            </CardTitle>
+          </Card>
+        ))}
+      </div>
+    </div>
   )
 }
