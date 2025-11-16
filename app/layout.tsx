a726f13769b83ba202fd51eae1f0b9dc46906747
SeungsooChoi <tmdtn5389@gmail.com>
2025-11-16 17:29:37 +0900

    chore: import 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Navigation from '@/components/navigation';
+import Navigation from '@/components/Navigation';
 
 export const metadata: Metadata = {
   title: '하루 단어(一日の単語)',
